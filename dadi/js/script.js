98e11cd9ee3d7b856d90f84596b647c1acb875c4
Ghezlene Doballah <ghyzounette@gmail.com>
2020-01-07 17:29:56 +0100

    inizio gioco

---
+++ dadi/js/script.js
@@ -0,0 +1,9 @@
+alert('ciao!');
+var num1 = [Math.floor(Math.random() * 6) + 1];
+// console.log(num1);
+var num2 = [Math.floor(Math.random() * 6) + 1];
+// console.log(num2);
+for (var i = 0; i<num1.length; i++) {
+var numero1 = num1[i];
+console.log(num1);
+}

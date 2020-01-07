14f0bf2b59bf83e12b50619382516f7ea84ffd82
Ghezlene Doballah <ghyzounette@gmail.com>
2020-01-07 18:55:14 +0100

    final commit dado

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,9 +1,25 @@
-alert('ciao!');
-var num1 = [Math.floor(Math.random() * 6) + 1];
-// console.log(num1);
-var num2 = [Math.floor(Math.random() * 6) + 1];
-// console.log(num2);
-for (var i = 0; i<num1.length; i++) {
-var numero1 = num1[i];
-console.log(num1);
+alert('Benvenuti al gioco!');
+var buttonLancio = document.getElementById('lancio');
+buttonLancio.addEventListener('click',
+function() {
+  var dado1 = document.getElementById('dado1');
+  var dado2 = document.getElementById('dado2');
+  var status = document.getElementById('status');
+  var num1 = Math.floor(Math.random() * 6) + 1;
+  console.log(num1);
+  var num2 = Math.floor(Math.random() * 6) + 1;
+  console.log(num2);
+  dado1.innerHTML = num1;
+  dado2.innerHTML = num2;
+  status.innerHTML = 'hai generato ' + num1 + ' , ' + num2 + '.';
+  if (num1>num2) {
+  status.innerHTML += 'HAI VINTO!';
+  }
+  else if (num1<num2) {
+  status.innerHTML += ' HAI PERSO!';
+  }
+   else {
+   status.innerHTML += ' PUOI RILANCIARE!';
+  }
 }
+);
